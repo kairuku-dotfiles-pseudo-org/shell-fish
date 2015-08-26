e70e0b396d4843372a662e204bd093c022acbbfd
Derrick G <ddgvcs@gmail.com>
2015-08-26 18:33:47 -0500

    add msg to a git func; delete extraneous bak file

--- config/workstation/generic/ln_src_func_shared/functions/open/git_oriented/gits.fish
+++ config/workstation/generic/ln_src_func_shared/functions/open/git_oriented/gits.fish
@@ -4,5 +4,6 @@ function gits -d "git status alias"
     git status
     __cli_message "Invoke 'git diff' or 'git log' if needed"
     __cli_message "If the last commit was a mistake, call 'git_undo_last_commit'"
+    __cli_message "Otherwise, call 'git_add_commit_push_tracked' when ready"
 
 end
